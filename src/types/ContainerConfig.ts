88863465c5ad4a33aa09f50c732d89299ab79280
Jannik Wibker <jannikwibker@gmail.com>
2022-05-29 15:18:58 +0200

    overcomplicated container type even more!

--- src/types/ContainerConfig.ts
+++ src/types/ContainerConfig.ts
@@ -8,6 +8,35 @@ const string_array: D.Decoder<unknown, string[]> = pipe(
   D.parse(s => D.success(s.split(',')))
 )
 
+const container_build_config = D.union(
+  /**
+   * docker method:
+   * pulls changes using git and then rebuilts containers using docker-compose build
+   * */
+  D.struct({ method: D.literal('docker'),  working_directory: D.string }), // TODO: git related things?
+  /**
+   * ansible method:
+   * runs the corresponding ansible playbook which in turn does all deployment related things such as pulling git repos, building containers and starting them
+   */
+  D.struct({ method: D.literal('ansible'), working_directory: D.string }), // TODO: which playbook, on which host, ...?
+  /**
+   * custom command:
+   * just executes the custom command that is supplied (using a shell)
+   */
+  D.struct({ method: D.literal('command'), working_directory: D.string, command: D.string  }) // TODO: some kind of input arguments?
+)
+
+const container_webhook_config = D.union(
+  /**
+   * uses github webhooks
+   */
+  D.struct({ method: D.literal('github'), secret: D.string }),
+  /**
+   * uses gitea webhooks
+   */
+  D.struct({ method: D.literal('gitea'),  secret: D.string }),
+)
+
 const container_config = D.intersect(
   D.partial({
     /**
@@ -29,42 +58,38 @@ const container_config = D.intersect(
     /**
      * deployment information
      */
-    build: D.union(
-      /**
-       * docker method:
-       * pulls changes using git and then rebuilts containers using docker-compose build
-       * */
-      D.struct({ method: D.literal('docker'),  working_directory: D.string }), // TODO: git related things?
-      /**
-       * ansible method:
-       * runs the corresponding ansible playbook which in turn does all deployment related things such as pulling git repos, building containers and starting them
-       */
-      D.struct({ method: D.literal('ansible'), working_directory: D.string }), // TODO: which playbook, on which host, ...?
-      /**
-       * custom command:
-       * just executes the custom command that is supplied (using a shell)
-       */
-      D.struct({ method: D.literal('command'), working_directory: D.string, command: D.string  }) // TODO: some kind of input arguments?
-    ),
+    build: container_build_config,
     /**
      * webhook information
      */
-    webhook: D.union(
-      /**
-       * uses github webhooks
-       */
-      D.struct({ method: D.literal('github'), secret: D.string }),
-      /**
-       * uses gitea webhooks
-       */
-      D.struct({ method: D.literal('gitea'),  secret: D.string }),
-    )
+    webhook: container_webhook_config
   })
 )
 
-export type ContainerConfig = D.TypeOf<typeof container_config>
+type FromEntries<Entries extends { method: string }> = {
+  [key in Entries['method']]: Entries extends { method: key } ? Entries : never
+}
 
-export type HarborContainer = {
+type Narrow<Source, Key extends string, New> = Source & {
+  [key in Key]: New
+}
+
+export type BuildTypesRaw = D.TypeOf<typeof container_build_config>
+export type WebhookTypesRaw = D.TypeOf<typeof container_webhook_config>
+
+export type BuildTypes = FromEntries<BuildTypesRaw>
+export type WebhookTypes = FromEntries<WebhookTypesRaw>
+
+export type ContainerConfigRaw = D.TypeOf<typeof container_config>
+
+export type ContainerConfig<
+  NarrowWebhook extends WebhookTypesRaw = WebhookTypesRaw,
+  NarrowBuild extends BuildTypesRaw = BuildTypesRaw
+> = Narrow<Narrow<ContainerConfigRaw, 'webhook', NarrowWebhook>, 'build', NarrowBuild>
+
+export type HarborContainer<
+  NarrowWebhook extends WebhookTypesRaw = WebhookTypesRaw,
+  NarrowBuild extends BuildTypesRaw = BuildTypesRaw> = {
   container: {
     id: string,
     image: {
@@ -73,7 +98,7 @@ export type HarborContainer = {
     },
     state: string
   },
-  config: ContainerConfig
+  config: ContainerConfig<NarrowWebhook, NarrowBuild>
 }
 
 export {
